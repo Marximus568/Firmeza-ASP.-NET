01e3db833610b431671e07a9ae0f44bd596d28e0
anonyma <TheresNotBe@protonmail.com>
2025-11-12 18:23:50 -0500

    New attribute: to clients

--- AdminDashboard.Domain/Entities/Clients.cs
+++ AdminDashboard.Domain/Entities/Clients.cs
@@ -16,8 +16,9 @@ public class Clients : Person
     public string Address { get; set; } = string.Empty;
     
     [MaxLength(20)]
-    public string Role { get; set; } = string.Empty;
+    public string Role { get; set; } = "Customer";
 
+    public bool EmailConfirmed { get; set; } = false;
     // Navigation property: One client can have many sales
     public ICollection<Sales> Sales { get; set; } = new List<Sales>();
     
